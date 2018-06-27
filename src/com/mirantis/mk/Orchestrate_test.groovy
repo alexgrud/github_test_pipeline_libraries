29a1baccc1e67ba94eb37f242649a340f0497c21
Oleksii Grudev <ogrudev@mirantis.com>
2018-06-27 12:52:17 +0300

    sync

--- src/com/mirantis/mk/Orchestrate_test.groovy
+++ src/com/mirantis/mk/Orchestrate_test.groovy
@@ -22,7 +22,7 @@ def RunTestGetConfig(master, config) {
 
 @NonCPS
 def SortFunction() {
-    def sampleMap2 = ["aaa":1,"bbbb":40,"ccc":60]
+    def sampleMap2 = ["aaa":100,"bbbb":40,"ccc":60]
     def sortedMap = sampleMap2.sort {it.value}
     return sortedMap
 }
