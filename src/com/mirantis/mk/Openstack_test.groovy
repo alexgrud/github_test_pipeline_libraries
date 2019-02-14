96183a530f5a5c774e7d2565ed86976617ae867c
Oleksii Grudev <ogrudev@mirantis.com>
2019-02-14 18:04:17 +0200

    return to correct pillar key

--- src/com/mirantis/mk/Openstack_test.groovy
+++ src/com/mirantis/mk/Openstack_test.groovy
@@ -450,9 +450,10 @@ def getOpenStackUpgradeServices(env, target){
         Map<String,Integer> _sorted_apps = [:]
         for (k in global_apps['return'][0].values()[0].keySet()) {
             if (k in node_apps) {
-                if (node_pillar['return'][0].values()[k]['upgrades']['enabled'][0] != null) {
-                    if (node_pillar['return'][0].values()[k]['upgrades']['enabled'][0].toBoolean())
+                if (node_pillar['return'][0].values()[k]['upgrade']['enabled'][0] != null) {
+                    if (node_pillar['return'][0].values()[k]['upgrades']['enabled'][0].toBoolean()) {
                       _sorted_apps[k] = global_apps['return'][0].values()[0][k].values()[0].toInteger()
+                    }
                 }
             }
         }
